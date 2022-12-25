990c8aeae2666f229e266250222d7c9093822d3b
Timo <smartie8@hotmail.de>
2022-12-25 22:42:50 +0100

    Minor dependency changes, implemented adding new anamnesia data

--- src/routes/Anamnesia.js
+++ src/routes/Anamnesia.js
@@ -3,16 +3,13 @@ const router = express.Router();
 const MySQL = require("../database/MySQL");
 const Session = require("../services/SessionManagerService");
 const Patient = require("../routes/Patient");
-const JWT = require("jsonwebtoken");
 
 router.get("/",async (req, res) => {
     //Get all Anamnesia Data belonging to a user
     if(req.body.session && Session.verifyToken(req.body.session)){
         const profile = Session.verifyToken(req.body.session);
         if(profile){
-            console.log(profile.email)
             let id = await Patient.getUserID(profile.email);
-            console.log(id)
             if(id){
                 MySQL.query('SELECT content FROM anamnesia_filled WHERE user_owner=?',id,(err, rows) => {
                     if(err){
@@ -27,8 +24,22 @@ router.get("/",async (req, res) => {
 });
 
 
-router.post("/",(req,res) => {
+router.post("/",async (req,res) => {
+    //Add new user anamnesia data
+    if(req.body.session && Session.verifyToken(req.body.session)){
+        const profile = Session.verifyToken(req.body.session);
+        if(profile){
+            let id = await Patient.getUserID(profile.email);
+            if(id){
+                MySQL.query('INSERT INTO anamnesia_filled (content,user_owner) VALUES (?,?)',[req.body.content,id],(err, rows) => {
+                    if(err){
+                        res.sendStatus(500);
+                    } else res.sendStatus(200);
+                });
+            }
 
+        }
+    }
 })
 
 
--- src/routes/VerifyAccount.js
+++ src/routes/VerifyAccount.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const MySQL = require("../database/MySQL");
-const Session = require("../services/SessionManagerService");
-const Password = require("../services/PasswordHasherService");
-const Email = require("../services/EmailSenderService");
-const crypto = require("crypto");
-const e = require("express");
 
 
 router.get("/",(req, res) => {
