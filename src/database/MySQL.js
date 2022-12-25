91b9a868ce91c8c28d84c9527f4c8dc0969093a8
Timo <smartie8@hotmail.de>
2022-12-25 23:01:36 +0100

    Put MySQL Credentials into .env

--- src/database/MySQL.js
+++ src/database/MySQL.js
@@ -1,10 +1,10 @@
 const mySQL = require('mysql');
 
 let config = {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'anamnaid'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 };
 
 const connection = mySQL.createConnection(config);
