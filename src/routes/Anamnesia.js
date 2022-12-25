652b41e4bbeb570c4b68095215bdeaf127144c1c
Timo <smartie8@hotmail.de>
2022-12-25 22:59:30 +0100

    Added relationship between anamnesia data and the doctor it belongs to

--- src/routes/Anamnesia.js
+++ src/routes/Anamnesia.js
@@ -5,19 +5,31 @@ const Session = require("../services/SessionManagerService");
 const Patient = require("../routes/Patient");
 
 router.get("/",async (req, res) => {
-    //Get all Anamnesia Data belonging to a user
+    //Get Anamnesia Data belonging to a user
     if(req.body.session){
         const profile = Session.verifyToken(req.body.session);
         if(profile){
             let id = await Patient.getUserID(profile.email);
             if(id){
-                MySQL.query('SELECT content FROM anamnesia_filled WHERE user_owner=?',id,(err, rows) => {
-                    if(err){
-                        res.sendStatus(500);
-                    } else {
-                        res.send(rows);
-                    }
-                });
+                //If doctor ID is provided, return anamnesia data which is related to the provided doctor
+                if(req.body.doctor){
+                    MySQL.query('SELECT content FROM anamnesia_filled WHERE user_owner=? AND doctor=?', [id,req.body.doctor], (err, rows) => {
+                        if (err) {
+                            res.sendStatus(500);
+                        } else {
+                            res.send(rows);
+                        }
+                    });
+                } else {
+                    //Return all anamnesia data belonging to an user
+                    MySQL.query('SELECT content FROM anamnesia_filled WHERE user_owner=?', id, (err, rows) => {
+                        if (err) {
+                            res.sendStatus(500);
+                        } else {
+                            res.send(rows);
+                        }
+                    });
+                }
             } else res.sendStatus(500);
         } else res.sendStatus(500);
     } else res.sendStatus(403);
@@ -31,7 +43,7 @@ router.post("/",async (req,res) => {
         if(profile){
             let id = await Patient.getUserID(profile.email);
             if(id){
-                MySQL.query('INSERT INTO anamnesia_filled (content,user_owner) VALUES (?,?)',[req.body.content,id],(err, rows) => {
+                MySQL.query('INSERT INTO anamnesia_filled (content,user_owner,doctor) VALUES (?,?,?)',[req.body.content,id,req.body.doctor],(err, rows) => {
                     if(err){
                         res.sendStatus(500);
                     } else res.sendStatus(200);
