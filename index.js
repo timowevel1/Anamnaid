29dcc249a66ac145ab7a2000dcb138d7ad0ffadf
Timo <smartie8@hotmail.de>
2022-12-27 16:44:04 +0100

    Minor changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const app = require('./src/Routes');
 
 app.listen(process.env.PORT, () => {
-    console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+    console.log(`Anamnaid Backend listening at http://localhost:${process.env.PORT}`);
 });
 
 module.exports.PORT = process.env.PORT;
